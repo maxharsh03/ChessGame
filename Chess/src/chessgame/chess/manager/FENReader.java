e8336d94f699068e70965d234eae7fdb8713ae3a
mlharshb <mlharshb@ncsu.edu>
2023-05-04 22:29:51 -0400

    Can now move pieces on Board.

--- Chess/src/chessgame/chess/manager/FENReader.java
+++ Chess/src/chessgame/chess/manager/FENReader.java
@@ -1,8 +1,13 @@
 package chessgame.chess.manager;
 
+import chessgame.chess.board.Board;
+import chessgame.chess.piece.Color;
+import chessgame.chess.piece.Pawn;
+
 /**
  * Class that reads in FEN chess notation. Allows for games to easily be read in to 
- * the chess system. 
+ * the chess system. All methods are static and there is no constructor because 
+ * there is no need to ever construct a FENReader object. 
  * @author maxharsh
  *
  */
@@ -12,6 +17,38 @@ public class FENReader {
 	 * Reads FEN string in
 	 */
 	public static void readFEN(String fenStr) {
+		Board board = new Board();
+		int count = 0;
 		
+		for(int i = 0; i < fenStr.length(); i++) {
+			for(int j = 0; j < 8; j++) {
+				
+			}
+			if(fenStr.substring(i, i + 1).equals("p")) {
+				board.getBoard()[i][i] = new Pawn(Color.BLACK, 0, 0);
+			} else if(fenStr.substring(i, i + 1).equals("r")) {
+				
+			} else if(fenStr.substring(i, i + 1).equals("b")) {
+				
+			} else if(fenStr.substring(i, i + 1).equals("n")) {
+				
+			} else if(fenStr.substring(i, i + 1).equals("q")) {
+				
+			} else if(fenStr.substring(i, i + 1).equals("k")) {
+				
+			} else if(fenStr.substring(i, i + 1).equals("P")) {
+				
+			} else if(fenStr.substring(i, i + 1).equals("R")) {
+				
+			} else if(fenStr.substring(i, i + 1).equals("B")) {
+				
+			} else if(fenStr.substring(i, i + 1).equals("N")) {
+				
+			} else if(fenStr.substring(i, i + 1).equals("K")) {
+				
+			} else if(fenStr.substring(i, i + 1).equals("Q")) {
+				
+			}
+		}
 	}
 }
--- Chess/src/chessgame/chess/ui/ChessGUI.java
+++ Chess/src/chessgame/chess/ui/ChessGUI.java
@@ -6,8 +6,11 @@ import java.awt.Dimension;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -21,30 +24,43 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 import chessgame.chess.board.Board;
 import chessgame.chess.manager.GameManager;
 import chessgame.chess.piece.Piece;
 
-import java.awt.color.*;
-
 public class ChessGUI{
 
     private static final long serialVersionUID = 1L;
     private GameManager gm = new GameManager();
+    private Board board = gm.getBoard();
     private JFrame frame;
     private BoardPanel boardPanel;
+    private static final int FRAME_WIDTH = 600;
+    private static final int FRAME_HEIGHT = 600;
+    private static final int ROW = 8;
+    private static final int COL = 8;
+    
+    private static final Dimension FRAME_DIMENSION = new Dimension(FRAME_WIDTH, FRAME_HEIGHT);
+    
+    private Point initialClick;
+    private Point finalClick;
+    private Piece pieceToMove;
 
     public ChessGUI() {
         this.frame = new JFrame("Chess") ;
         this.frame.setLayout(new BorderLayout());
         JMenuBar tableMenuBar = createTableMenuBar();
         this.boardPanel = new BoardPanel();
+        this.boardPanel.setLocation(0, 0);
         
+        //this.frame.setLocation(0, 0);
         this.frame.setJMenuBar(tableMenuBar);
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.frame.setSize(600, 600);
+        this.frame.setPreferredSize(FRAME_DIMENSION);
         this.frame.add(this.boardPanel, BorderLayout.CENTER);
+        this.frame.pack();
         this.frame.setVisible(true);
     }
    
@@ -91,42 +107,127 @@ public class ChessGUI{
     		this.boardTiles = new ArrayList<TilePanel>();
     		for(int i = 0; i < 8; i++) {
     			for(int j = 0; j < 8; j++) {
-    				TilePanel tilePanel = new TilePanel(this, i + j, gm.getBoard().getPieceFromBoard(i, j));
+    				TilePanel tilePanel = new TilePanel(this, i, j);
     				this.boardTiles.add(tilePanel);
     				add(tilePanel);
     			}
     		}
-    		setPreferredSize(new Dimension(10, 10));
     		validate();
+    	}
+    	
+    	public void drawBoard() {
+    		removeAll();
+    		for(TilePanel tilePanel : boardTiles) {
+    			tilePanel.drawTile();
+    			add(tilePanel);
+    		}
+    		validate();
+    		repaint();
     	}
     }
     
     private class TilePanel extends JPanel {
     	private int tileId;
-    	private Piece piece;
+    	private int row;
+    	private int col;
     	
-    	public TilePanel(BoardPanel boardPanel, int tileId, Piece piece) {
+    	public TilePanel(BoardPanel boardPanel, int row, int col) {
     		super(new GridBagLayout());
-    		this.tileId = tileId;
-    		this.piece = piece;
-    		setPreferredSize(new Dimension(10, 10));
+    		this.tileId = row + col;
+    		this.row = row;
+    		this.col = col;
+    		setPreferredSize(new Dimension(FRAME_WIDTH/ROW, FRAME_HEIGHT/COL));
     		assignTileColor();
     		assignImage();
+    		
+    		addMouseListener(new MouseListener() {
+    			
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					// cancels piece selection
+					if(SwingUtilities.isRightMouseButton(e)) {
+						initialClick = null;
+						finalClick = null;
+						System.out.println(row + "," + col);
+					} 
+					// cancels piece selection
+					else if(SwingUtilities.isLeftMouseButton(e)) {
+						if(initialClick == null) {
+							initialClick = new Point(row, col);
+						} else {
+							finalClick = new Point(row, col);
+							// after a valid move, reset the final and initial clicks
+							System.out.println("Initial Click: " + initialClick.getX() + "," + initialClick.getY());
+							System.out.println("Final Click: " + finalClick.getX() + "," + finalClick.getY());
+
+							if(gm.makeMove((int)initialClick.getX(), (int)initialClick.getY(), (int)finalClick.getX(), (int)finalClick.getY())) {
+								initialClick = null;
+								finalClick = null;
+								
+								SwingUtilities.invokeLater(new Runnable() {
+									@Override
+									public void run() {
+										boardPanel.drawBoard();
+									}
+								});
+							} else {
+								// make new pop-up window detailing error
+								initialClick = null;
+								finalClick = null;
+								throw new IllegalArgumentException("Invalid move. Try again.");
+							}
+						}
+					}
+				}
+
+				@Override
+				public void mousePressed(MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+
+				@Override
+				public void mouseReleased(MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+
+				@Override
+				public void mouseEntered(MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+
+				@Override
+				public void mouseExited(MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+    		});
+    		
     		validate();
     	}
 
+    	public void drawTile() {
+    		removeAll();
+    		assignTileColor();
+    		assignImage();
+    		validate();
+    		repaint();
+    	}
+    	
 		private void assignTileColor() {
 			boolean isLight = ((this.tileId) % 2) == 0;
-			setBackground(isLight ?  Color.white : Color.gray);
+			setBackground(isLight ?  new Color(64, 47, 29) : new Color(210, 180, 140));
 		}
 		
 		private void assignImage() { 
-			if(this.piece != null) {
+			if(board.getPieceFromBoard(row, col) != null) {
 				try {
-					BufferedImage image = ImageIO.read(new File(piece.getImage()));
-					add(new JLabel(new ImageIcon(image.getScaledInstance(75, 75, Image.SCALE_DEFAULT))));
+					BufferedImage image = ImageIO.read(new File(board.getPieceFromBoard(row, col).getImage()));
+					add(new JLabel(new ImageIcon(image.getScaledInstance(75, 75, Image.SCALE_DEFAULT)), JLabel.CENTER));
 				} catch (IOException e) {
-					throw new IllegalArgumentException();
+					// skip, this exception should never occur
 				}
 			}
 		}
